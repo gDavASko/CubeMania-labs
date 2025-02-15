3c66904f50f1674e97daf493abc9a135bdf0b5cc
gDavASko <DavASko01@gmail.com>
2025-02-16 02:49:20 +0500

    - gen sands and resources

--- Assets/Scripts/Planetary/RantimeGeneratorS.cs
+++ Assets/Scripts/Planetary/RantimeGeneratorS.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -8,24 +9,45 @@ using Random = Unity.Mathematics.Random;
 
 namespace GDB.Planetary
 {
+    public enum GenStep
+    {
+        GenSandPoints = 0,
+        GenSandCubes = 1,
+        GenResourcesPoints = 2,
+        GenResourceCube = 3,
+        GetFinish = 1000,
+    }
+    
     public partial class RantimeGeneratorS : SystemBase
     {
         // Создаем NativeList для результата (плоский список узлов)
-        private HashSet<float3> resultNodes = new();
-        private Queue<float3> positions;
-        private uint lastIndex;
+        private Dictionary<float3, Entity> sandNodes = new();
+        private Dictionary<float3, Entity> resourceNodes = new();
+        
         private Queue<(uint, float3)> queueNodes = new();
+
+        private GenStep curStep;
 
         private float3 center = new float3(0, 0, 0);
 
+        private int curBox = 0;
+        private int curResourceBox = 0;
+
+        private int maxResources = 10;
+        private int curResCount = 0;
+
         private bool completedPoints;
+        private bool completedResourcesPoints;
         private bool completedCubes;
+
+        private bool initedResources;
 
         private EntityCommandBuffer ecb;
 
-        Entity baseCube = Entity.Null;
-        Entity sandCube = Entity.Null;
-        Entity rockCube = Entity.Null;
+        private Entity baseCube = Entity.Null;
+        private Entity sandCube = Entity.Null;
+        private Entity ResourceCube = Entity.Null;
+        private Entity rockCube = Entity.Null;
 
         private readonly float3[] directions = new float3[6]
         {
@@ -41,22 +63,22 @@ namespace GDB.Planetary
         protected override void OnCreate()
         {
             base.OnCreate();
-            lastIndex = 0;
+            curStep = GenStep.GenSandPoints;    
             completedPoints = false;
+            completedResourcesPoints = false;
             completedCubes = false;
-            queueNodes.Enqueue((0, center));
+            initedResources = false;
 
-            /*ecb = SystemAPI
-                .GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(EntityManager.World.Unmanaged);*/
+            curResCount = 0;
+            queueNodes.Enqueue((0, center));
         }
 
         protected override void OnUpdate()
         {
             ecb = World.GetOrCreateSystemManaged<EndSimulationEntityCommandBufferSystem>()
                 .CreateCommandBuffer();
-            
-            if (completedPoints && completedCubes)
+
+            if (curStep == GenStep.GetFinish)
                 return;
 
             if (baseCube == Entity.Null)
@@ -68,7 +90,7 @@ namespace GDB.Planetary
 
                     foreach (var c in prefabBuffer)
                     {
-                        if (c.cubeType == CubeType.Resource)
+                        if (c.cubeType == CubeType.Ice)
                         {
                             baseCube = c.Prefab;
                         }
@@ -80,71 +102,164 @@ namespace GDB.Planetary
                         {
                             rockCube = c.Prefab;
                         }
+                        else if (c.cubeType == CubeType.Resource)
+                        {
+                            ResourceCube = c.Prefab;
+                        }
 
-                        if (baseCube != Entity.Null && sandCube != Entity.Null && rockCube != Entity.Null)
+                        if (baseCube != Entity.Null && sandCube != Entity.Null && rockCube != Entity.Null &&
+                            ResourceCube != Entity.Null)
                         {
                             break;
                         }
                     }
                 }
-                
+
                 // Создаем центральный блок вручную
                 CreateCube(center, baseCube, ecb);
-                TryProcessTreeNode(2, 4, 6);
+                TryProcessTreeNode(2, 6, 6);
             }
 
             if (baseCube == Entity.Null)
                 return;
 
-            if (!completedPoints)
+            switch (curStep)
             {
-                uint maxRadius = 10;
-                uint maxNeighbors = 4;
-                uint minNeighbors = 2;
+                case GenStep.GenSandPoints:
+                    uint maxRadius = 100;
+                    uint maxNeighbors = 5;
+                    uint minNeighbors = 1;
 
-                if (!TryProcessTreeNode(maxRadius, minNeighbors, maxNeighbors))
+                    if (!TryProcessTreeNode(maxRadius, minNeighbors, maxNeighbors))
+                    {
+                        curStep = GenStep.GenSandCubes;
+                        curBox = 0;
+                        queueNodes.Clear();
+                        Debug.LogError("Completed points!");
+                    }
+
+                    break;
+
+                case GenStep.GenSandCubes:
+                    var point = sandNodes.Keys.ElementAt(curBox);
+                    sandNodes[point] = CreateCube(point, sandCube, ecb);
+                    curBox++;
+
+                    if (curBox >= sandNodes.Count)
+                        curStep = GenStep.GenResourcesPoints;
+                    break;
+
+                case GenStep.GenResourcesPoints:
+                    uint maxBlocks = 10;
+                    uint maxResNeighbors = 4;
+                    uint minResNeighbors = 1;
+
+                    if (!TryAddResourceBlocks(maxBlocks, minResNeighbors, maxResNeighbors))
+                    {
+                        curBox = 0;
+                        curStep = GenStep.GenResourceCube;
+                    }
+
+                    break;
+
+                case GenStep.GenResourceCube:
+
+                    var resPoint = resourceNodes.Keys.ElementAt(curBox);
+
+                    curBox++;
+                    if (resourceNodes[resPoint] != Entity.Null)
+                        return;
+                    
+                    resourceNodes[resPoint] = CreateCube(resPoint, ResourceCube, ecb);
+
+                    if (curBox >= resourceNodes.Count)
+                    {
+                        curResCount++;
+                        if (curResCount >= maxResources)
+                        {
+                            curStep = GenStep.GetFinish;
+                            Debug.LogError("Completed cubes!");
+                        }
+                        else
+                        {
+                            resourceNodes.Clear();
+                            queueNodes.Clear();
+                            initedResources = false;
+                            curStep = GenStep.GenResourcesPoints;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        public bool TryAddResourceBlocks(uint maxBlocks, uint minNeighbors, uint maxNeighbors)
+        {
+            if (sandNodes.Count == 0 || maxBlocks == 0)
+                return false;
+
+            Random random = new Random((uint)System.DateTime.UtcNow.Millisecond + 2);
+            
+            if (!initedResources)
+            {
+                int startIndex = random.NextInt(0, sandNodes.Count);
+                float3 startPos = sandNodes.Keys.ElementAt(startIndex);
+
+                if (resourceNodes.ContainsKey(startPos))
+                    return true;
+                
+                queueNodes.Enqueue((0, startPos));
+                resourceNodes[startPos] = Entity.Null;
+                initedResources = true;
+            }
+
+            if(queueNodes.Count > 0)
+            {
+                var (curDepth, curPos) = queueNodes.Dequeue();
+                
+                if (curDepth >= maxBlocks)
+                    return true;
+        
+                uint neighborsCount = random.NextUInt(minNeighbors, maxNeighbors);
+                List<uint> neighbors = new();
+
+                while (neighbors.Count < neighborsCount)
                 {
-                    positions = new Queue<float3>(resultNodes);
-                    completedPoints = true;
-                    Debug.LogError("Completed points!");
+                    uint newIndex = random.NextUInt(0, (uint)directions.Length);
+                    if (neighbors.Contains(newIndex))
+                        continue;
+                    neighbors.Add(newIndex);
                 }
+
+                foreach (var neighbor in neighbors)
+                {
+                    if (resourceNodes.Count >= maxBlocks)
+                        continue;
+                    
+                    float3 newPoint = curPos + directions[(int)neighbor];
+
+                    if (resourceNodes.ContainsKey(newPoint))
+                        continue;
+
+                    if (sandNodes.ContainsKey(newPoint))
+                    {
+                        if (sandNodes[newPoint] != Entity.Null)
+                        {
+                            ecb.DestroyEntity(sandNodes[newPoint]);
+                        }
+
+                        sandNodes.Remove(newPoint);
+                    }
+                    
+                    queueNodes.Enqueue((curDepth + 1, newPoint));
+                    resourceNodes[newPoint] = Entity.Null;
+                    
+                    Debug.LogError($"Added resource node: {newPoint} for dist {curDepth + 1}");
+                }
+
+                return true;
             }
-            else if(positions.Count > 0)
-            {
-                CreateCube(positions.Dequeue(), sandCube, ecb);
-            }
-            else
-            {
-                Debug.LogError("Completed cubes!");
-                completedCubes = true;
-            }
 
-            /*// Преобразуем плоский список в координаты
-             NativeArray<float3> resultPoints = new NativeArray<float3>(resultNodes.Length, Allocator.TempJob);
-             for (int i = 0; i < resultNodes.Length; i++)
-             {
-                 resultPoints[i] = resultNodes[i].position;
-             }
-
-             Debug.Break();
-             
-
-            /*var ecbSystem = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
-            EntityCommandBuffer ecb2 = ecbSystem.CreateCommandBuffer(state.World.Unmanaged);
-
-            var spawnJob = new SpawnCubesJob
-            {
-                positions = resultPoints,
-                ecb = ecb2.AsParallelWriter(),
-                cubePrefab = sandCube
-            };
-
-            // Запускаем Job
-            JobHandle jobHandle2 = spawnJob.Schedule(resultPoints.Length, 64); // Параллельное выполнение
-            jobHandle2.Complete(); // Завершаем Job перед использованием ECB
-
-
-            Debug.LogError("Completed2!");*/
+            return false;
         }
         
         public bool TryProcessTreeNode(uint maxRadius, uint minNeighbors, uint maxNeighbors)
@@ -171,20 +286,19 @@ namespace GDB.Planetary
                 
                 neighbors.Add(newIndex);
             }
-
+            
             foreach(var neighbor in neighbors)// Добавляем узел в плоский список
             {
-                lastIndex++;
                 float3 point = curPos + directions[(int)neighbor];
                 queueNodes.Enqueue((curRadius + 1, point));
-                resultNodes.Add(point);
+                sandNodes[point] = Entity.Null;
                 //Debug.LogError($"Added node: {point} for dist {curRadius + 1}");
             }
 
             return true;
         }
 
-        private void CreateCube(float3 pos, Entity cube, EntityCommandBuffer cBuffer)
+        private Entity CreateCube(float3 pos, Entity cube, EntityCommandBuffer cBuffer)
         {
             Entity spawnedEntity = cBuffer.Instantiate(cube);
 
@@ -196,6 +310,7 @@ namespace GDB.Planetary
             });
             
             //Debug.LogError($"Created cube: index {spawnedEntity.Index} for point {pos}");
+            return spawnedEntity;
         }
     }
 }
